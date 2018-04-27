2b6f6f41672386c41c56f1ce4c105d63f2c3d5a9
timmelander <tmelander@gmail.com>
2018-04-27 14:54:57 -0400

    Updates to script

---
+++ standalonescript/R2PS3/OHS-WG/setEnv.sh
@@ -0,0 +1,16 @@
+# Set Environment
+
+export ENV="DEV"
+export INSTALL_ROOT="/u01"
+export INSTALL_BASE=$INSTALL_ROOT/app
+export MW_HOME=$INSTALL_BASE/oracle/middleware
+export OHS_HOME=$MW_HOME/Oracle_OHS
+export OHS_INSTANCE=$OHS_HOME/instances/instance1/config/OHS/ohs1
+export WG1_HOME=$MW_HOME/Oracle_WG1
+export RREG_TEMPLATE="RREG_AGENT_TEMPLATE.xml"
+export RREG_AGENT11G="RREG_OAM11G_Request.xml"
+export RREG_AGENT12C="RREG_OAM12C_Request.xml"
+export OAMHOST="oam1.melander.us"
+export OHSHOST=`hostname`
+export DOMAIN_USERNAME=weblogic
+export DOMAIN_PASSWORD=Oracle123
